9c33bf2dfaf1945a8d0d11078f165b2147e51b86
DavidHigpen <davidhigpen@gmail.com>
2024-04-09 20:56:33 -0500

    new updates

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Send-Emails
+
 This program is used for sending out a mass of emails to different parishes to advertise the Connect Retreat
+
+## Email-Setup
+
+1. 
+
+## Installation
+
+1. Clone this repo to your local machine.
+```bash
+git clone https://github.com/DavidHigpen/Send-Emails.git
+```
+
+2. Install libary: `python-dotenv` or change code to have Google API key not hidden.
+```bash
+pip install python-dotenv
+```
+
+3. If using .env, create a new file in same directory called `.env`
+
+4. Go to https://myaccount.google.com/u/1/signinoptions/two-step-verification and sign in with sender email account 
+
+5. Hit "App Passwords" and create a new app password and same it something relevent
+
+6. Copy the 16 character password and in your .env file, enter PASSWORD='<your_password>'
+
+7. Run the main program file: `python3 sendEmails.py`.
--- sendEmails.py
+++ sendEmails.py
@@ -2,68 +2,124 @@ import os
 import sys
 import ssl
 import smtplib
+import csv
 from dotenv import load_dotenv
-from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+
+#CONVERT ’ to ' for all messages!
 
 def to_multiline_string(normal_string):
     return f"""{normal_string}"""
+    
 
-def get_parishes():
-    file_path = './parishes.txt'
-    parishes = []
-    emails = []
-    with open(file_path, 'r') as file:
-        text = file.read()
-        text = text.split("\n")
-        for parish in text:
-            parish = parish.split(", ")
-            parishes.append(parish[0])
-            emails.append(parish[1])
-    return parishes, emails
+# def get_parishes():
+#     file_path = './parishes.txt'
+#     parishes = []
+#     emails = []
+#     with open(file_path, 'r') as file:
+#         text = file.read()
+#         text = text.split("\n")
+#         for parish in text:
+#             parish = parish.split(", ")
+#             parishes.append(parish[0])
+#             emails.append(parish[1])
+#     return parishes, emails
+
+def getFromCSV():
+    diocesesSheets = os.listdir('./dioceses')
+    parish_names = []
+    secretary_emails = []
+    youth_minister_names = []
+    youth_minister_emails = []
+    bulletin_emails = []
+    for dioceses in diocesesSheets:
+        diocesesPath = os.path.join('./dioceses', dioceses)
+        
+        with open(diocesesPath, newline='') as sheet:
+            
+            firstRow = True
+            csvReader = csv.reader(sheet)
+            for row in csvReader:
+                if(not firstRow):
+                    if row[7].find('@') != -1 and row[2]:
+                        parish_names.append(row[2])
+                        secretary_emails.append(row[7])
+                    if row[11].find('@') != -1 and row[10]:
+                        youth_minister_names.append(row[10])
+                        youth_minister_emails.append(row[11])
+                firstRow = False
+    return parish_names, secretary_emails, bulletin_emails, youth_minister_names, youth_minister_emails
 
 def load_configuration():
     load_dotenv()
     sender = 'davidhigpen@gmail.com' # Enter your email here
     password = os.environ.get('PASSWORD') # Update password in .env file in same dir
-    parishes, emails = get_parishes()
-    file_path = "./message.txt"
-    return sender, password, emails, parishes, file_path
+    parish_names, secretary_emails, bulletin_emails, youth_minister_names, youth_minister_emails = getFromCSV()
+    return sender, password, parish_names, secretary_emails, bulletin_emails, youth_minister_names, youth_minister_emails
 
-def load_messages(file_path, parishes):
+def load_messages(file_path, insertIntoHotword, hotword):
     with open(file_path, 'r') as file:
         text = file.read()
-        messages = [to_multiline_string(text.replace('[parish]', parish)) for parish in parishes]
+        messages = [to_multiline_string(text.replace(hotword, replacement)) for replacement in insertIntoHotword]
     return messages
 
-def send_emails(sender, password, emails, parishes, messages):
-    for index, (body, receiver, parish) in enumerate(zip(messages, emails, parishes)):
-        subject = f'Welcome {parish} students to Connect!'
-        body += f"\n\nThis would have sent to {receiver}"
-        receiver = 'davidislearninghowtosendemails@gmail.com' # DELETE ME to send out emails
-        attachments = ["TestAttachment1.jpg"]
-
-        em = EmailMessage()
+def send_emails(sender, password, emails, receiver_names, messages):
+    for index, (body, receiver_email, receiver_name) in enumerate(zip(messages, emails, receiver_names)):
+        subject = 'Connect Retreat 2024'
+        body += f"\n\nThis would have sent to {receiver_email}" # DELETE ME when sending emails
+        receiver_email = 'davidislearninghowtosendemails@gmail.com' # DELETE ME to send out emails
+        attachments = ["TestAttachment1.jpg"] # Change to add relevant attachments
+        
+        with open('./signiture.txt', 'r') as sig_file:
+            signiture = sig_file.read()
+            
+        signitureLogo = './connectLogo.png'
+        
+        em = MIMEMultipart()
 
         em['From'] = sender
-        em['To'] = receiver
+        em['To'] = receiver_email
         em['Subject'] = subject
-        em.set_content(body)
-        for attachment_path in attachments:
-            with open(attachment_path, "rb") as attachment:
-                em.add_attachment(attachment.read(), maintype="application", subtype='octet-stream', filename=os.path.basename(attachment_path))
+        
+        emBody = MIMEText(body, 'plain')
+        em.attach(emBody)
+        
+        emHTML = MIMEText(signiture % (signitureLogo), 'html')   
+        em.attach(emHTML)   # Added, and edited the previous line
 
+        with open(signitureLogo, 'rb') as fp:
+            img = MIMEImage(fp.read())
+        img.add_header('Content-ID', '<{}>'.format(signitureLogo))
+        em.attach(img)
 
+        # for attachment_path in attachments:
+        #     with open(attachment_path, "rb") as attachment:
+        #         em.attach(attachment.read(), maintype="application", subtype='octet-stream', filename=os.path.basename(attachment_path))
+        
+        
         context = ssl.create_default_context()
-
         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
             smtp.login(sender, password)
-            smtp.sendmail(sender, receiver, em.as_string())
-        print(f"Email sent to {parish}")
+            smtp.sendmail(sender, receiver_email, em.as_string())
+            
+        print(f"Email sent to {receiver_name}")
+        # exit() # DELETE ME
 
 def main():
-    sender, password, emails, parishes, file_path = load_configuration()
-    messages = load_messages(file_path, parishes)
-    send_emails(sender, password, emails, parishes, messages)
+    sender, password, parish_names, secretary_emails, bulletin_emails, youth_minister_names, youth_minister_emails = load_configuration()
+
+    secretary_messages = load_messages('./parishMessage.txt', parish_names, '[NO_HOTWORD]')
+    send_emails(sender, password, secretary_emails, parish_names, secretary_messages)
+    
+    youth_minister_messages = load_messages('./youthMinister.txt', youth_minister_names, '[Youth Minister Name]')
+    send_emails(sender, password, youth_minister_emails, youth_minister_names, youth_minister_messages)
+    
+    bulletin_messages = load_messages('./bulletin.txt', parish_names, '[Parish Name]')
+    send_emails(sender, password, bulletin_emails, bulletin_emails, bulletin_messages)
+    
     sys.exit()
 
 if __name__ == "__main__":
