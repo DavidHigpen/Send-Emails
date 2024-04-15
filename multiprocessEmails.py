2b2e11c25fb21df65b7c0e2fbc5307f9f9e753be
DavidHigpen <davidhigpen@gmail.com>
2024-04-14 21:59:23 -0500

    updates including bcc support

--- multiprocessEmails.py
+++ multiprocessEmails.py
@@ -14,19 +14,17 @@ def main():
     semaphore = Semaphore(max_requests)
     worker_pool = []
         
-    sender, password, parish_names, secretary_emails, bulletin_emails, bulletin_parishs, youth_minister_names, youth_minister_emails = load_configuration()
+    sender, password, parish_names, secretary_emails, youth_minister_names, youth_minister_emails = load_configuration()
 
     secretary_messages = load_messages('./parishMessage.txt', parish_names, '[NO_HOTWORD]')
     youth_minister_messages = load_messages('./youthMinister.txt', youth_minister_names, '[Youth Minister Name]')
-    bulletin_messages = load_messages('./bulletin.txt', bulletin_parishs, '[Parish Name]')
+    # bulletin_messages = load_messages('./bulletin.txt', bulletin_parishs, '[Parish Name]')
     
-    email_queue = secretary_emails + youth_minister_emails + bulletin_emails
-    name_queue = parish_names + youth_minister_names + bulletin_parishs
-    message_queue = secretary_messages + youth_minister_messages + bulletin_messages
-    category_queue = [' (secretary)'] * len(secretary_emails) + [' (youth minister)'] * len(youth_minister_emails) + [' (bulletin)'] * len(bulletin_emails)
+    email_queue = secretary_emails + youth_minister_emails
+    name_queue = parish_names + youth_minister_names
+    message_queue = secretary_messages + youth_minister_messages
+    category_queue = [' (secretary)'] * len(secretary_emails) + [' (youth minister)'] * len(youth_minister_emails)
     
-    # print(email_queue)
-    # print(name_queue)
     print(len(message_queue))
     
         
